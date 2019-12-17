7f96f2b2d14d387b47476a077914cb14626a3ef9
Damian <dga1999@wp.pl>
2019-12-17 15:07:19 +0100

    Tworzenie opcji dodawania darów..

--- src/main/java/pl/coderslab/charity/controllers/DonationController.java
+++ src/main/java/pl/coderslab/charity/controllers/DonationController.java
@@ -7,21 +7,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import pl.coderslab.charity.dtos.DonationDataDTO;
 import pl.coderslab.charity.repositories.CategoryRepository;
+import pl.coderslab.charity.repositories.InstitutionRepository;
 
 @Controller
 @RequestMapping("/donation")
 public class DonationController {
 
     private final CategoryRepository categoryRepository;
+    private final InstitutionRepository institutionRepository;
 
-    public DonationController(CategoryRepository categoryRepository) {
+    public DonationController(CategoryRepository categoryRepository, InstitutionRepository institutionRepository) {
         this.categoryRepository = categoryRepository;
+        this.institutionRepository = institutionRepository;
     }
 
 
     @GetMapping("/form")
     public String prepareDonation(Model model){
         model.addAttribute("donationData", new DonationDataDTO());
+        model.addAttribute("allInstitution", institutionRepository.findAll());
         return "form";
+        //<form:select path="institution" items="${allInstitution}" itemLabel="name" itemValue="id" multiple="true"/>
     }
 }
