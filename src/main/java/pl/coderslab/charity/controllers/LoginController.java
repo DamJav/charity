200679335c53299c4d1401ccae01f75022675527
Damian <dga1999@wp.pl>
2019-12-19 14:19:24 +0100

    Rejestracja działająca

---
+++ src/main/java/pl/coderslab/charity/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package pl.coderslab.charity.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+
+    @GetMapping
+    public String prepareLoginPage(){
+        return "login";
+    }
+}
+
--- src/main/java/pl/coderslab/charity/controllers/RegistrationController.java
+++ src/main/java/pl/coderslab/charity/controllers/RegistrationController.java
@@ -37,6 +37,6 @@ public class RegistrationController {
             return "register";
         }
         registrationService.register(registrationData);
-        return "redirect:/login";
+        return "redirect:/";
     }
 }
