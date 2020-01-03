da453b30570e86cc40fc819ca6b5e714b4374bd6
Damian <dga1999@wp.pl>
2020-01-03 11:24:18 +0100

    Strony dostepne tylko po zalogowaniu

--- src/main/java/pl/coderslab/charity/config/SecurityConfiguration.java
+++ src/main/java/pl/coderslab/charity/config/SecurityConfiguration.java
@@ -35,17 +35,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         "= ur.user_id JOIN roles r ON ur.roles_id = r.id WHERE u.email = ?");
     }
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                // .antMatchers("/user", "/user/*", "/user/**")
-                .antMatchers("/**").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/resources/**").permitAll()
                 .antMatchers("/register").permitAll()
                 .antMatchers("/login").permitAll()
-                .antMatchers("/media/**").permitAll()
-                .antMatchers("/more/**").permitAll()
-                .antMatchers("/logout").authenticated()
-                .antMatchers("/advertisements/**").permitAll()
                 .antMatchers("/user", "/user/**").hasRole("USER")
                 .antMatchers("/admin", "/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
